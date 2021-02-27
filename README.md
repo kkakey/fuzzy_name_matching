4c0eea11e99c8c813fa311d7f05d1468ab25295c
kkakey <kkakey999@gmail.com>
2021-02-27 13:47:46 -0500

    add R file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# fuzzy_name_matching
+# fuzzy name matching
+
+Matches similar names in one list to another using fuzzy logic in R.
+
+Adapted from [Darius Mehri's](https://github.com/dariusmehri/Name-Matching-Algorithm-using-Fuzzy-Logic) python name matching algorithm
---
+++ fuzzy_matching.R
@@ -0,0 +1,55 @@
+###############################################################  
+### Fuzzy name matching algorithm
+###############################################################  
+
+# R version adapted from Darius Mehri's python name matching algorithm:
+# https://github.com/dariusmehri/Name-Matching-Algorithm-using-Fuzzy-Logic
+
+###############################################################  
+# nameList -- list of names to find matches for
+# nl -- list of names to search for matches in
+# threshold -- name matching threshold (0-1)
+# one.top.match -- keep only the top name match (T) or all name matches above threshold (F)
+###############################################################  
+
+fuzzy_name <- function(nameList, nl, threshold, one.top.match){
+  library(fuzzywuzzyR)
+  matched_list <- matrix(0, nrow=length(nameList))
+  
+  for (j in (1:length(nameList))) {
+
+    ratio_list <- c()
+    for (i in (1:length(nl)) ) {
+      #calculates a matching score
+      init = SequenceMatcher$new(string1 = nameList[j], string2 = nl[i])
+      ratio = init$ratio()
+      
+      name <- nl[i]
+      #add the name to ration_list and its score
+      ratio_list <- c(ratio_list, list(c(name, ratio)))
+      
+      if (i == (length(nl)-1)) {
+        #sort score from highest to lowest
+        ind <- with(ratio_list, order(sapply(ratio_list, "[[", 2), decreasing = T))
+        ratio_list = ratio_list[ind]
+      }
+      # only keep match names of a certain threshold
+      if (ratio_list[[1]][2] >= threshold) {
+        if (one.top.match) {
+          # chooses one top match
+          matched_list[j,] <- ratio_list[[1]][1]
+        }
+        # all name matches that are above the threshold
+        else {
+          top_matches <- ratio_list[sapply(ratio_list, "[[", 2) >= threshold]
+          ml <- sapply(top_matches, "[[", 1)
+          matched_list[j,] <- paste(unique(ml), collapse = '; ')
+        }
+      }
+    }
+    # print(paste0("nameList: ", nameList[j]))
+    # print(paste0("matched_list: ", matched_list[j]))
+  }
+  output <- as.data.frame(cbind(nameList, matched_list))
+  return(output)
+}
